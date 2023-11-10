09d4cd9ee19fbcceb67d864f0590d305d1f0c0d2
petrovkarmm <petrovleonid1999@mail.ru>
2023-11-10 15:20:58 +0500

    creating post_search view

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from taggit.models import Tag
@@ -30,6 +31,30 @@ def post_comment(request, post_id):
     return render(request, 'blog/post/comment.html',
                   {'post': post, 'form': form, 'comment': comment})
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # Взвешивание запроса, первостепенный на title при поиске, второе место body.
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
 
 def post_share(request, post_id):
     # Извлечь пост по идентификатору id
